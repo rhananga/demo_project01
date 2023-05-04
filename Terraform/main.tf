874667afb1adf9a199813f9aa0dd728c7bd66095
rhananga <107822688+rhananga@users.noreply.github.com>
2023-05-04 17:58:44 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,20 +5,20 @@ provider "aws" {
 }
 
 # main.tf
-resource "aws_vpc" "example_vpc" {
+resource "aws_vpc" "testvpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "example_subnet" {
-  vpc_id     = aws_vpc.example_vpc.id
+resource "aws_subnet" "testsubnet" {
+  vpc_id     = aws_vpc.testvpc.id
   cidr_block = "10.0.1.0/24"
 }
 
 # output.tf
 output "vpc_id" {
-  value = aws_vpc.example_vpc.id
+  value = aws_vpc.testvpc.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.example_subnet.id
+  value = aws_subnet.testsubnet.id
 }
